6c29809150d4c2e1377f74dcf2d1ef5750ba5d51
Erik Dwornik <dwer1011@h-ka.de>
2022-10-07 16:04:27 +0200

    removed code for git demo

--- co2_sensor.py
+++ co2_sensor.py
@@ -8,8 +8,7 @@ client = mqtt.Client("Co2_level")
 client.connect(mqttBroker)
 
 while True:
-    randNumber = uniform(20.0, 21.0)
+    randNumber = uniform(1000.0, 3500.0)
     client.publish("CO2LEVEL", randNumber)
     print("Just published " + str(randNumber) + " to topic CO2LEVEL")
-    time.sleep(1)
-    # print("alles kaputt Rolf")
+    time.sleep(60)
