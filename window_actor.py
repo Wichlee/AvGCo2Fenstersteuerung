f82becb314bc2a48299495ad0b9fcccac6966163
Erik Dwornik <dwer1011@h-ka.de>
2022-10-10 15:40:35 +0200

    added parameter client and userdata to function on_method

--- window_actor.py
+++ window_actor.py
@@ -1,5 +1,4 @@
 import paho.mqtt.client as mqtt
-import time
 
 mqttBrokerHostname = "localhost"
 mqttBrokerPort = 1883
@@ -7,15 +6,16 @@ WindowIsOpen = False
 client_id = "window_actor"
 
 
-def on_message(message):
-    co2_level = message.payload
-    print("recieved message: ", co2_level)
+def on_message(client, userdata, message):
+
+    print("recieved message: ", message.payload)
     print("message topic: ", message.topic)
 
-    if co2_level >= 1500.0:
+    if message.payload >= 1500.0:
         print("Opening Window")
         WindowIsOpen = True
-    if co2_level < 1500.0:
+
+    if message.payload < 1500.0:
         print("Closing Window")
         WindowIsOpen = False
 
